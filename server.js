825844f85165ee0bbedd4ebfbe6909473a306212
pnamani3 <pnamani3@gatech.edu>
2021-11-24 19:21:29 +0000

    error on spelling i hope is the fix

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.post('/api/shorturl/', bodyParser.urlencoded({ extended: false }) , (request
   let urlRegex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi)
   
   if(!inputUrl.match(urlRegex)){
-    response.json({error: 'Invalid URL'})
+    response.json({error: 'invalid url'})
     return
   }
     
